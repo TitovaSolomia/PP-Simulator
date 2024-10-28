37d64576a26216d52195d3981b8d1a7b238625e9
Titova Solomia <titovasolomia@icloud.com>
2024-10-28 23:42:40 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Creature
+{
+    private string name = "Unknown";
+    private int level = 1;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public Creature() { }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"My name is {Name}, my level is {Level}.");
+    }
+
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,5 +5,24 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+
+        Creature a = new Creature();
+
+        a.SayHi();
+
+        a.Name = "P.Diddy";
+        a.Level = 4;
+
+        Console.WriteLine(a.Info);
+
+
+        Animals animal = new Animals { Description = "Dogs" };
+
+        Console.WriteLine(animal.Info);  
+
+        animal.Size = 5;
+        Console.WriteLine(animal.Info);  
     }
+
+    
 }
